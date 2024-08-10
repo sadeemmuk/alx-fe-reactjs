d13b04f2ce6ccc1cc88c1f9507749bcfb737dce3
sadeemmuk <sadeemmuk2012@gmail.com>
2024-08-10 10:04:42 +0300

    added UserProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './WelcomeMessage'
 import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
+import UserProfile from './UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,6 +37,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
     </>
     
   )
