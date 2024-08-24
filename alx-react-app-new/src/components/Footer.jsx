2010395d32376bf81b9fe97f61b1d1a8e171d30e
sadeemmuk <sadeemmuk2012@gmail.com>
2024-08-24 20:43:41 +0300

    added new repo

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <footer>
-        <p>&#169;2023 City Lovers</p>
+        <p style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>&#169;2023 City Lovers</p>
         </footer>
     )
 }
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent (){
     return(
-        <main style={{border: '1px solid gray', padding: '10px', margin: '10px'}}>
+        <main>
         <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     )
