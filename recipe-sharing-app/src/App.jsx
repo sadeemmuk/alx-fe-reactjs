c9c4c2a4b0da125ec1eb56dc7f10067529ca4ab8
Sadeem <106956864+sadeemmuk@users.noreply.github.com>
2024-09-01 23:58:47 +0300

    Add files via upload

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+function App() {
+  return (
+    <>
+    <RecipeList />
+    <AddRecipeForm/>
+    </>
+  )
+}
+
+export default App
