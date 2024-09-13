3f7d5642d290cdaf7c363ec8ce7f7132a1430cad
sadeemmuk <sadeemmuk2012@gmail.com>
2024-09-13 08:21:27 +0300

    responsive design

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -19,13 +19,13 @@ const HomePage = () => {
 
 
     return(
-        <div className="w-1/2 ml-auto mr-auto mt-20 flex ">
+        <div className="w-1/2 ml-auto mr-auto mt-20 grid grid-cols-1 grid-cols-2 sm:grid-row-1 sm:grid-row-2 md:w-full  ">
         <>
         {
         
             data.map((dataItem)=>(
-                <div key={dataItem.id} className=" mr-20 hover:bg-teal-200 shadow-lg text-center rounded p-10">
-                    <image src={dataItem.image}></image>
+                <div key={dataItem.id} className=" mr-20 hover:bg-teal-200 shadow-lg text-center rounded p-10 sm:w-500 text-center">
+                    <img src={dataItem.image} alt="Logo" />
                     <h2 className="text-xl font-bold">{dataItem.title}</h2>
                     <p className="text-base">{dataItem.summary}</p>
                 </div>
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
+    screens: {
+      'tablet': '640px',
+      'laptop': '1024px',
+      'desktop': '1280px'
+    },
     extend: {},
   },
   plugins: [],
